e2a05bf72a6753a4a8514eae299b74c8920bd577
bupt_yyx <2647194133@qq.com>
2022-05-10 14:55:52 +0800

    5/10 FT代码重构前

--- functional_tests_3.py
+++ functional_tests_3.py
@@ -20,22 +20,20 @@ class NewVisitorTest(unittest.TestCase):
 
         #she is invited to enter a to-do item straight way
         inputbox=self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        inputbox.send_keys('Buy peacok feathers')
-
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text=='1:Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn(
+            '1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',[row.text for row in rows]
         )
-
         #
         self.fail('Finish the test!')
 
--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
+    # if request.method=='POST':
+    #     return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html',{
+        'new_item_text':request.POST.get('item_text',''),
+    })
     # return HttpResponse('<html><title>To-Do lists</title></html>')
