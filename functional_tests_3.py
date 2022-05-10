86eaa29691287cd23c2a74ed2d8650a671b7f9a2
bupt_yyx <2647194133@qq.com>
2022-05-10 13:14:57 +0800

    5/10

--- functional_tests_3.py
+++ functional_tests_3.py
@@ -30,9 +30,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_element_by_tag_name('tr')
+        rows=table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text=='1:Buy peacock feathers' for row in rows)
+            any(row.text=='1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         #
