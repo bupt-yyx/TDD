bde25434d8109424846336dab22a409cdc5fc6fc
bupt_yyx <2647194133@qq.com>
2022-05-17 08:40:19 +0800

    5/17 添加模型item，通过单元测试

--- lists/tests.py
+++ lists/tests.py
@@ -15,7 +15,7 @@ from django.template.loader import render_to_string
 #         self.assertIn('<title>To-Do lists</title>',html)
 #         self.assertTrue(html.endswith('</html>'))
 from lists.views import home_page
-
+from lists.models import Item
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -36,3 +36,21 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response,'home.html')
         # self.assertTemplateUsed(response,'wrong.html')
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_item(self):
+        first_item=Item()
+        first_item.text='The first (ever) list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
