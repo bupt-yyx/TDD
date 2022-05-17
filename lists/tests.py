3a962ce61d614a98cacaa8d3f82e701d9081c30b
bupt_yyx <2647194133@qq.com>
2022-05-17 09:53:23 +0800

    5/17 重定位

--- lists/tests.py
+++ lists/tests.py
@@ -22,9 +22,24 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response,'home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/',data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def text_redirects_after_POST(self):
         response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
-        self.assertTemplateUsed(response,"home.html")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_lists_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response=self.client.get('/')
+        self.assertIn('itemey 1',response.content.decode())
+        self.assertIn('itemey 2',response.content.decode())
 
     def test_home_page_returns_correct_html(self):
         response=self.client.get('/')
@@ -36,6 +51,11 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response,'home.html')
         # self.assertTemplateUsed(response,'wrong.html')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_item(self):
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     # if request.method=='POST':
     #     return HttpResponse(request.POST['item_text'])
-    return render(request,'home.html',{
-        'new_item_text':request.POST.get('item_text',''),
-    })
+    if request.method=='POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items=Item.objects.all()
+    return render(request,'home.html',{'items':items})
     # return HttpResponse('<html><title>To-Do lists</title></html>')
