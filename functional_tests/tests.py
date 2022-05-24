5932b59799cba77680db0b8c0cba510f441f03c5
bupt_yyx <2647194133@qq.com>
2022-05-24 08:19:50 +0800

    5/24把功能测集成进功能中

---
+++ functional_tests/tests.py
@@ -0,0 +1,47 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser=webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit()
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heaed about a cool new online to-do app.She goes
+
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the test!')
+
+
