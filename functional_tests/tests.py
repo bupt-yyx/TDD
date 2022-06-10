2d34076981130f91618e8c2719c7cb103d1fddfe
bupt_yyx <2647194133@qq.com>
2022-06-10 11:07:24 +0800

    重构html

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -84,5 +84,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk',page_text)
 
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
 
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,
+            512,
+            delta=10
+        )
 
