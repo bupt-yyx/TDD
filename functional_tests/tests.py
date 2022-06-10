687b96a695f2ac15cfc027217a9bb3810a6188f2
bupt_yyx <2647194133@qq.com>
2022-06-10 13:50:36 +0800

    使用bootstrap提高layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -6,7 +7,7 @@ import time
 import unittest
 MAX_WAIT=10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser=webdriver.Chrome()
     def tearDown(self):
